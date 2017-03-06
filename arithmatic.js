0074c4e00bfe7e4a0c175b4754328cb8b9ff4195
Bhupendra Singh Parihar <bhupendraparihar@outlook.com>
2017-03-07 01:18:24 +0530

    Create arithmatic.js

---
+++ arithmatic.js
@@ -0,0 +1,16 @@
+const arithmatic = (function(){
+  return {
+    plus : function(op1,op2){
+      return op1 + op2;
+    },
+    minus: function(op1,op2){
+      return op1 - op2;
+    },
+    multiply: function(op1,op2){
+      return op1 * op2;
+    },
+    divide: function(op1,op2){
+      return op1 / op2;
+    }
+  }
+}());
